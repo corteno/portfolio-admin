7309be81585b4ff2d917d1e334b8378e1dd99423
Dávid <corten.mgs@gmail.com>
2017-10-02 16:08:32 +0200

    - Soemthing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Switch} from 'react-router-dom';
 
 import './index.css';
 import Login from './components/login';
